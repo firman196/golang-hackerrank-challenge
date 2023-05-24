c1d7fbe13fa3dd9cce8258d117d855f0dc9cd01a
firman196 <firmanagussaputro970@gmail.com>
2023-05-24 21:47:48 +0700

    add time convertion challenge

---
+++ time_convertion.go
@@ -0,0 +1,35 @@
+package main
+
+import (
+	"fmt"
+	"strconv"
+	"strings"
+)
+
+func timeConvertion(s string) string {
+	am := strings.HasSuffix(s, "AM")
+	pm := strings.HasSuffix(s, "PM")
+	arrTime := strings.Split(s, ":")
+	hh := arrTime[0]
+	mm := arrTime[1]
+	arrSs := strings.Split(arrTime[2], "")
+	ss := arrSs[0] + arrSs[1]
+	intHh, _ := strconv.Atoi(hh)
+
+	if am && intHh == 12 {
+		intHh = 0
+	}
+
+	if pm && intHh != 12 {
+		intHh += 12
+	}
+
+	return fmt.Sprintf("%02s:%02s:%02s", strconv.Itoa(intHh), mm, ss)
+}
+
+func main() {
+	result1 := timeConvertion("12:40:22AM")
+	fmt.Println(result1)
+	result2 := timeConvertion("07:05:45PM")
+	fmt.Println(result2)
+}
